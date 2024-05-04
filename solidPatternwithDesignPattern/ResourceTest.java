f317e48b4f2c4db75f853126ad65d75c9c77f70b
Aeron Red <aeronredcelajes@gmail.com>
2024-05-04 20:48:25 +0800

    SOLIDwithDesignPattern code here

--- solidPatternwithDesignPattern/ResourceTest.java
+++ solidPatternwithDesignPattern/ResourceTest.java
@@ -5,10 +5,10 @@ import java.util.*;
 public class ResourceTest {
     public static void main(String[] args) {
         Map<String, BorrowAction> resourceTypes = new HashMap<>();
-        resourceTypes.put("book", new ResourceTypeBorrow());
-        resourceTypes.put("journal", new ResourceTypeBorrow());
-        resourceTypes.put("thesis", new ResourceTypeBorrow());
-        resourceTypes.put("audio book", new ResourceTypeBorrow());
+        resourceTypes.put("book", new ResourceType());
+        resourceTypes.put("journal", new ResourceType());
+        resourceTypes.put("thesis", new ResourceType());
+        resourceTypes.put("audio book", new ResourceType());
         // This is where adding more resourceTypes as needed for the future
 
         Student student = new Student(resourceTypes);
--- solidPatternwithDesignPattern/ResourceType.java
+++ solidPatternwithDesignPattern/ResourceType.java
@@ -1,6 +1,6 @@
 package solidPatternwithDesignPattern;
 
-public class ResourceType implements ResourceTypeBorrow {
+public class ResourceType implements BorrowAction {
     @Override
     public void borrow(Resources resources, String title) {
         System.out.println("Borrowing " + resources.getResourceType() + ": " + title);
