fb6517d5bcf8827d140aeb4da257fb6041f52076
Aeron Red <aeronredcelajes@gmail.com>
2024-05-04 20:46:18 +0800

    SOLIDwithDesignPattern code here

--- solidPatternwithDesignPattern/ResourceTest.java
+++ solidPatternwithDesignPattern/ResourceTest.java
@@ -8,6 +8,7 @@ public class ResourceTest {
         resourceTypes.put("book", new ResourceTypeBorrow());
         resourceTypes.put("journal", new ResourceTypeBorrow());
         resourceTypes.put("thesis", new ResourceTypeBorrow());
+        resourceTypes.put("audio book", new ResourceTypeBorrow());
         // This is where adding more resourceTypes as needed for the future
 
         Student student = new Student(resourceTypes);
@@ -15,9 +16,11 @@ public class ResourceTest {
         Resources book = new Resources("book", "Don Quixote");
         Resources journal = new Resources("journal", "Diary of a Wimpy Kid");
         Resources thesis = new Resources("thesis", "Einstein's thesis");
+        Resources audioBook = new Resources("audio book", "Funny Story");
 
         student.borrowResource(book);
         student.borrowResource(journal);
         student.borrowResource(thesis);
+        student.borrowResource(audioBook);
     }
 }
