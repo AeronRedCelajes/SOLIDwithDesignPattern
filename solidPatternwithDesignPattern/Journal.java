c9bcbf98a8efacbfce9aa66c6796fb5b6a94dafa
Aeron Red Celajes <142370807+AeronRedCelajes@users.noreply.github.com>
2024-05-04 17:05:21 +0800

    Delete solidPatternwithDesignPattern/Journal.java

--- solidPatternwithDesignPattern/Journal.java
+++
@@ -1,8 +0,0 @@
-package solidPatternwithDesignPattern;
-
-public class Journal implements ResourceTypeBorrow {
-    @Override
-    public void borrow(String title) {
-        System.out.println("Borrowing journal: " + title);
-    }
-}
