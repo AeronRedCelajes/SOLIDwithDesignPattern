3ec610399d798dfae4cc34929e9d6898978bf476
Aeron Red Celajes <142370807+AeronRedCelajes@users.noreply.github.com>
2024-05-09 10:48:50 +0800

    Delete solidPatternwithDesignPattern/Book.java

--- solidPatternwithDesignPattern/Book.java
+++
@@ -1,7 +0,0 @@
-package solidPatternwithDesignPattern;
-
-public class Book implements ResourceBorrow{
-    public void borrowResource(Student student, ResourceType resourceType) {
-        System.out.println(student.getName() + " is borrowing a " + resourceType.getResourceType() + ": " + resourceType.getTitle());
-    }
-}
